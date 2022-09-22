25b94da97be22fac2832897e9cb65694cd0bf68f
hyeseong <hyeseong.ee@gmail.com>
2022-09-22 11:31:23 +0900

    config 리팩토링

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,9 @@ export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 export const GUEST_TOKEN = process.env.REACT_APP_GUEST_TOKEN;
 
 export const endpoints = {
-  USER_BACKEND_API_GRAPHQL_ENDPOINT: IS_PRODUCTION
+  user_backend_api_graphql_endpoint: IS_PRODUCTION
     ? 'https://user-backend-api.trevari.co.kr/graphql'
     : process.env.REACT_APP_BACKEND_GQL || '',
+  user_login_page_url: IS_PRODUCTION ? 'https://login.trevari.co.kr' : 'http://localhost:3001',
+  user_login_endpoint: IS_PRODUCTION ? 'https://login-api.trevari.co.kr/graphql' : 'http://localhost:10000/graphql',
 };
