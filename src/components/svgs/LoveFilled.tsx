309889f5398e20d8f7888441908fac50ef6d37c3
hyeseong <hyeseong.ee@gmail.com>
2022-09-22 11:29:40 +0900

    svgs 추가

--- src/components/svgs/LoveFilled.tsx
+++ src/components/svgs/LoveFilled.tsx
@@ -4,16 +4,17 @@ interface IProps {
   width?: number;
   height?: number;
   color?: string;
+  strokeColor?: string;
 }
 
-const LoveFilled = ({ width = 24, height = 24, color = '#000' }: IProps) => {
+const LoveFilled = ({ width = 24, height = 24, color = '#FF7900', strokeColor = '#000' }: IProps) => {
   return (
     <svg width={width} height={height} viewBox="0 0 24 24" fill="none">
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9.33436 2.5H5.13939L2 5.7153V11.9679L12 21.6976L22 11.9679V5.7153L18.8606 2.5H14.6656L12 4.77508L9.33436 2.5Z"
+        d="M11.5627 6.21065L12.0312 6.58547L12.4998 6.21065L14.9506 4.25H18.0956L20.3125 6.46691V11.6759L12.0312 19.4701L3.75 11.6759V6.46691L5.96691 4.25H9.11191L11.5627 6.21065Z"
         fill={color}
+        stroke={strokeColor}
+        strokeWidth="1.5"
       />
     </svg>
   );
--- src/components/svgs/LoveOutline.tsx
+++ src/components/svgs/LoveOutline.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
+
 interface IProps {
   width?: number;
   height?: number;
   color?: string;
 }
 
-const LoveOutline = ({ width = 24, height = 24, color = '#6E6E6C' }: IProps) => {
+const LoveOutline = ({ width = 24, height = 24, color = '#000' }: IProps) => {
   return (
     <svg width={width} height={height} viewBox="0 0 24 24" fill="none">
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M5.03408 2.25H9.42654L12 4.44641L14.5735 2.25H18.9659L22.25 5.61349V12.0735L12 22.0464L1.75 12.0735V5.61349L5.03408 2.25ZM5.66592 3.75L3.25 6.22434V11.4401L12 19.9536L20.75 11.4401V6.22434L18.3341 3.75H15.1265L12 6.41846L8.87346 3.75H5.66592Z"
-        fill={color}
+        d="M11.5627 6.21065L12.0312 6.58547L12.4998 6.21065L14.9506 4.25H18.0956L20.3125 6.46691V11.6759L12.0312 19.4701L3.75 11.6759V6.46691L5.96691 4.25H9.11191L11.5627 6.21065Z"
+        stroke={color}
+        strokeWidth="1.5"
       />
     </svg>
   );
