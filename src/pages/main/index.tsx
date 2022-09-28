1d112881c0607b78589ad6afb00f0d387f354009
kykim00 <jeongsaeki@likelion.org>
2022-09-28 17:30:44 +0900

    pixel page view event 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import HeroSlider from 'pages/main/components/HeroSlider';
 import CurationList from 'pages/main/components/CurationList';
 import Posts from 'pages/main/components/Posts';
 import FooterComp from 'components/layout/Footer';
+import MainHelmet from './components/MainHelmet';
+import { useAppDispatch, useAppSelector } from 'services/store';
+import { selectUserId } from 'services/auth/auth.store';
+import { pageView } from 'services/analytics/analytics.store';
 
 function Main() {
+  const userId = useAppSelector(selectUserId);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(pageView(userId));
+  });
   return (
     <>
+      <MainHelmet />
       <HeroSlider />
       <CurationList />
       <Posts />
