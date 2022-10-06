d76ea3bfa35c4dc1538a9e7ea218a6c14b16a45b
kykim00 <jeongsaeki@likelion.org>
2022-10-06 19:18:48 +0900

    앱에서 게스트일 시 알림 버튼 보이지 않도록 수정

--- src/pages/main/components/AlarmButton.tsx
+++ src/pages/main/components/AlarmButton.tsx
@@ -4,20 +4,40 @@ import Alarm from 'components/svgs/Alarm';
 import AlarmOn from 'components/svgs/AlarmOn';
 import { useMobileDetect } from 'hooks/useDetectMobile';
 import { postMessage } from 'utils/postMessage';
+import { useAppSelector } from '../../../services/store';
+import { selectAuthenticated } from '../../../services/auth/auth.store';
+
+interface IEvent {
+  detail: {
+    hasNewNotification: boolean;
+  };
+}
 
 const AlarmButton = () => {
   const [hasNewAlarm, setHasNewAlarm] = useState(false);
   const isApp = useMobileDetect().isApp();
+  const authenticated = useAppSelector(selectAuthenticated);
 
   useEffect(() => {
-    setHasNewAlarm(window.localStorage.getItem('unreadNoti') === 'true');
+    if (isApp) {
+      setHasNewAlarm(window.localStorage.getItem('unreadNoti') === 'true');
+      window.addEventListener('customEventForRefreshNotificationIcon', refreshNotificationIcon);
+    }
+    return () => {
+      if (isApp) return;
+      return window.removeEventListener('customEventForRefreshNotificationIcon', refreshNotificationIcon);
+    };
   }, []);
+
+  const refreshNotificationIcon = (event: IEvent) => {
+    setHasNewAlarm(event.detail.hasNewNotification);
+  };
 
   const onClickAlarm = () => {
     postMessage('TrevariFlutterAppForNotifications', 'true');
   };
 
-  if (!isApp) return <BarInstance></BarInstance>;
+  if (!isApp || !authenticated) return <BarInstance></BarInstance>;
 
   if (hasNewAlarm)
     return (
