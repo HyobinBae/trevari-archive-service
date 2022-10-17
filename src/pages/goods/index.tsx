7d756a255776fda95a197a31a741319a1e2daa38
hyeseong <hyeseong.ee@gmail.com>
2022-10-17 16:21:18 +0900

    이미지 URL 변경

--- src/pages/goods/index.tsx
+++ src/pages/goods/index.tsx
@@ -6,7 +6,7 @@ const Goods = () => {
   return (
     <>
       <ImageWrap
-        src="https://image.trevari.co.kr/file/fbfaaf08-46f7-4561-9fc8-2b982f8a3981.goods_detail.jpg"
+        src="https://image.trevari.co.kr/file/7196a19a-213b-44a4-bb98-d9b94d441c64.%E1%84%89%E1%85%B2%E1%84%91%E1%85%A5%E1%84%86%E1%85%A1%E1%84%8F%E1%85%A6%E1%87%80%20%E1%84%80%E1%85%AE%E1%86%BA%E1%84%8C%E1%85%B3%20%E1%84%89%E1%85%A1%E1%86%BC%E1%84%89%E1%85%A6%E1%84%91%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%8C%E1%85%B510.17.jpg"
         alt={'goods-detail'}
       />
       <FixedBottomButton
