22a8d8aeac9e9d25b83142ad3c62ad9dadaecfd1
hyeseong <hyeseong.ee@gmail.com>
2022-10-17 11:18:00 +0900

    menu 페이지 리팩토링

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -16,55 +16,73 @@ const Menu = () => {
     window.location.href = url;
   };
 
+  const menuItems = [
+    {
+      title: '모든 클럽 보기',
+      onClick: () => goToPage('/apply'),
+      badge: null,
+    },
+    {
+      title: '커뮤니티 이벤트',
+      onClick: () => goToPage('/apply'),
+      badge: null,
+    },
+    {
+      title: '다른 클럽 놀러가기',
+      onClick: () => goToPage('/meetings'),
+      badge: (
+        <BadgeWrap>
+          <Badge variant="filled" size="small" backgroundColor={orange900} color={white}>
+            멤버 Only
+          </Badge>
+        </BadgeWrap>
+      ),
+    },
+    {
+      title: hasPartnerMembership ? '파트너 어드민' : '파트너 지원하기',
+      onClick: () =>
+        goToPage(
+          hasPartnerMembership
+            ? 'https://partners.trevari.co.kr/'
+            : 'https://trevari.co.kr/blog/show?id=40ef561e-9b31-4a40-882b-02c83cc194c4',
+        ),
+      badge: (
+        <BadgeWrap>
+          <Badge variant="filled" size="small" backgroundColor={orange900} color={white}>
+            멤버 Only
+          </Badge>
+        </BadgeWrap>
+      ),
+    },
+    {
+      title: '슈퍼마켓',
+      onClick: () => goToPage('/goods'),
+      badge: (
+        <BadgeWrap>
+          <Badge variant="filled" size="small" backgroundColor={green900} color={white}>
+            NEW
+          </Badge>
+        </BadgeWrap>
+      ),
+    },
+    {
+      title: '블로그',
+      onClick: () => goToPage(`${endpoints.user_page_url}/blog`),
+      badge: null,
+    },
+  ];
   return (
     <MenuContainer>
-      <MenuItemAnchor onClick={() => goToPage('/apply')}>
-        <MenuItem>모든 클럽 보기</MenuItem>
-      </MenuItemAnchor>
-      <MenuItemAnchor onClick={() => goToPage('/events')}>
-        <MenuItem>커뮤니티 이벤트</MenuItem>
-      </MenuItemAnchor>
-      <MenuItemAnchor onClick={() => goToPage('/meetings')}>
-        <MenuItem>
-          다른 클럽 놀러가기
-          <BadgeWrap>
-            <Badge variant="filled" size="small" backgroundColor={orange900} color={white}>
-              멤버 Only
-            </Badge>
-          </BadgeWrap>
-        </MenuItem>
-      </MenuItemAnchor>
-      <MenuItemAnchor
-        onClick={() =>
-          goToPage(
-            hasPartnerMembership
-              ? 'https://partners.trevari.co.kr/'
-              : 'https://trevari.co.kr/blog/show?id=40ef561e-9b31-4a40-882b-02c83cc194c4',
-          )
-        }
-      >
-        <MenuItem>
-          {hasPartnerMembership ? '파트너 어드민' : '파트너 지원하기'}
-          <BadgeWrap>
-            <Badge variant="filled" size="small" backgroundColor={orange900} color={white}>
-              멤버 Only
-            </Badge>
-          </BadgeWrap>
-        </MenuItem>
-      </MenuItemAnchor>
-      <MenuItemAnchor onClick={() => goToPage('/goods')}>
-        <MenuItem>
-          슈퍼마켓
-          <BadgeWrap>
-            <Badge variant="filled" size="small" backgroundColor={green900} color={white}>
-              NEW
-            </Badge>
-          </BadgeWrap>
-        </MenuItem>
-      </MenuItemAnchor>
-      <MenuItemAnchor onClick={() => goToPage(`${endpoints.user_page_url}/blog`)}>
-        <MenuItem>블로그</MenuItem>
-      </MenuItemAnchor>
+      {menuItems.map(({ title, onClick, badge }) => {
+        return (
+          <MenuItemAnchor key={title} onClick={onClick}>
+            <MenuItem>
+              {title}
+              {badge}
+            </MenuItem>
+          </MenuItemAnchor>
+        );
+      })}
     </MenuContainer>
   );
 };
