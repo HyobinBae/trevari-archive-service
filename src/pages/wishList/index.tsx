e84b476704790f4141841c1bf36a2868604ce819
kykim00 <jeongsaeki@likelion.org>
2022-12-01 18:57:22 +0900

    wishList Loading 컴포넌트 추가

--- src/pages/wishList/index.tsx
+++ src/pages/wishList/index.tsx
@@ -7,21 +7,28 @@ import { goToPage } from 'utils';
 import { selectAuthenticated, selectUserId } from 'services/auth/auth.store';
 import { useAppDispatch, useAppSelector } from 'services/store';
 import { useWindowSize } from 'hooks/useWindowSize';
-import LoadingPage from 'components/base/LoadingPage';
 import Box from 'components/base/Box';
 import EmptyEmoji from 'components/svgs/EmptyEmoji';
 import NewCurationClubCard from 'pages/main/components/NewCurationClubCard';
 import { CURATION_CARD_ASPECT_RATIO } from 'pages/main/const';
-import { getWishClubs } from 'pages/main/services/main.api';
+import { getWishClubs, useGetWishClubsQuery } from 'pages/main/services/main.api';
 import { selectWishClubs } from 'pages/main/services/main.store';
+import { Loading } from '@trevari/components';
 
 const WishList = () => {
   const { width } = useWindowSize();
   const dispatch = useAppDispatch();
   const authenticated = useAppSelector(selectAuthenticated);
-  const wishClubs = useAppSelector(selectWishClubs);
-
   const userId = useAppSelector(selectUserId);
+  const { data: wishClubs, isLoading } = useGetWishClubsQuery({
+    where: {
+      userID: userId,
+      isClosed: false,
+      isFullClub: false,
+      isAppliablePeriod: true,
+    },
+  });
+
   const isGuest = userId === 'guest';
   useEffect(() => {
     if (isGuest) {
@@ -47,13 +54,18 @@ const WishList = () => {
       ? `calc(450px * ${CURATION_CARD_ASPECT_RATIO} / 2)`
       : `calc((100vw - 50px) * ${CURATION_CARD_ASPECT_RATIO} / 2)`;
 
-  if (!authenticated || !wishClubs) return <LoadingPage />;
+  if (!authenticated || isLoading)
+    return (
+      <LoadingContainer>
+        <Loading variant="gridCardList" />;
+      </LoadingContainer>
+    );
   return (
     <Box style={{ paddingTop: '48px', minHeight: '100vh', paddingBottom: '67px' }}>
-      <GridCardCount>{`총 ${wishClubs.length}개`}</GridCardCount>
-      {wishClubs.length > 0 ? (
+      <GridCardCount>{`총 ${wishClubs?.length}개`}</GridCardCount>
+      {wishClubs && wishClubs?.length > 0 ? (
         <GridBox>
-          {wishClubs.map(item => (
+          {wishClubs?.map(item => (
             <NewCurationClubCard
               isWishClub={true}
               cardWidth="100%"
@@ -97,5 +109,8 @@ const EmojiWrapper = styled.div`
 const EmptyDescription = styled.p`
   ${heading11};
   margin-top: 30px;
+`;
+const LoadingContainer = styled.div`
+  padding: 110px 20px;
 `;
 export default WishList;
