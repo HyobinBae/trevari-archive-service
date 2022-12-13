63af509b9520eb61f2489706470ef0fa8a8782bb
kykim00 <jeongsaeki@likelion.org>
2022-12-13 10:39:32 +0900

    parame으로 bookreview id 조회

--- src/pages/bookreviews/show.tsx
+++ src/pages/bookreviews/show.tsx
@@ -1,37 +1,29 @@
 import { Loading } from '@trevari/components';
-import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { selectAuthenticated } from 'services/auth/auth.store';
 import { useAppDispatch, useAppSelector } from 'services/store';
 import { selectUser } from 'services/user/user.store';
 import { useGetBookreviewQuery } from './services/api';
 import ProfileInBookreviewPage from 'pages/main/components/ProfileInBookreviewPage';
 import BookreviewContent from 'pages/main/components/BookreviewContent';
-import Comment from 'pages/main/components/Comment';
 import BookreviewComments from 'pages/main/components/BookreviewComments';
 
 const BookReviewShow = () => {
-  const { data: bookreview, isLoading } = useGetBookreviewQuery({ id: '9463e066-decb-4e05-8783-a5dd4b7caa83' });
+  const { bookreivewID } = useParams();
+  const { data: bookreview, isLoading } = useGetBookreviewQuery({ id: bookreivewID || '' });
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
   const authenticated = useAppSelector(selectAuthenticated);
-  const [commentInputState, setCommentInputState] = useState({
-    type: 'comment',
-    replyCommentID: '',
-    replyCommentUsername: '',
-  });
-  console.log(bookreview);
   useEffect(() => {
     // if (authenticated)
   }, []);
 
-  const onClickComment = (type: string, replyCommentID: string, replyCommentUsername: string) => {
-    setCommentInputState({ type, replyCommentID, replyCommentUsername });
-  };
   if (isLoading) return <Loading variant="gridCardList" />;
   return (
     <div>
       <ProfileInBookreviewPage
-        user={user}
+        user={bookreview?.user}
         clubName={bookreview?.club?.name || ''}
         publishedAt={bookreview?.publishedAt}
       />
