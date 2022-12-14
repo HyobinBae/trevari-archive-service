cfddba62116ab9f87252c1988ff24ac93d3bd760
saeromCho <saerom.r.cho@gmail.com>
2022-12-14 14:25:03 +0900

    푸터 구성 변경

--- src/components/layout/const.ts
+++ src/components/layout/const.ts
@@ -13,13 +13,7 @@ export interface IFooterTermsNav {
 export const FOOTER_NAVS: IFooterNav[] = [
   { link: `${endpoints.user_page_url}/cs`, title: '자주 묻는 질문' },
   { link: `${endpoints.user_page_url}/cs?tab=question`, title: '문의하기' },
-  { link: `${endpoints.user_page_url}/blog`, title: '블로그' },
   { link: `${endpoints.user_page_url}/recruit`, title: '트레바리 채용' },
-  {
-    link: `${endpoints.user_page_url}/blog/show?id=40ef561e-9b31-4a40-882b-02c83cc194c4`,
-    title: '파트너 모집',
-  },
-  { link: `${endpoints.user_page_url}/blog?category=공지`, title: '공지사항' },
 ];
 
 export const FOOTER_TERMS_NAV: IFooterTermsNav[] = [
@@ -29,14 +23,14 @@ export const FOOTER_TERMS_NAV: IFooterTermsNav[] = [
   },
   {
     link: `${endpoints.user_page_url}/policy?id=1`,
-    title: '개인정보처리방침',
+    title: '개인정보 처리 방침',
   },
   {
     link: `${endpoints.user_page_url}/policy?id=4c95dc6d-893e-430e-965e-d8a0f1521540`,
-    title: '트레바리 운영정책',
+    title: '운영정책',
   },
   {
-    link: `${endpoints.user_page_url}/blog?category=공지`,
-    title: '공지사항',
+    link: 'https://www.instagram.com/trevari_official/',
+    title: '인스타그램',
   },
 ];
