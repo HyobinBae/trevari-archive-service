62e8882b9abe9199362ecf562f2b9fc35ccc535b
nopecho <ch.jooon@gmail.com>
2022-12-16 12:34:47 +0900

    add subscription club curation desc

--- src/pages/main/components/NewCurationSubscriptionClubCard.tsx
+++ src/pages/main/components/NewCurationSubscriptionClubCard.tsx
@@ -3,7 +3,7 @@ import {
   DisplayCard,
   DisplayCardContent,
   DisplayCardHero,
-  DisplayCardMeetingInfo,
+  DisplayCardMeetingInfo, DisplayCardParagraph,
   DisplayCardPlaceInfo,
   DisplayCardTitle,
 } from '@trevari/business-components';
@@ -23,9 +23,11 @@ interface NewCurationClubCardProps {
 }
 
 const NewCurationSubscriptionClubCard = ({ subscriptionClub, cardWidth, imgHeight }: NewCurationClubCardProps) => {
+  console.log(subscriptionClub)
   const { id, content, liveSchedule } = subscriptionClub;
   const name = subscriptionClub.title;
-  const place = '온라인(Youtube)';
+  const desc = content.description;
+  const place = `온라인(${content.place})`;
   const schedule = formatOnlineClubsSchedule(liveSchedule.weekOfLiveSchedule, liveSchedule.dayOfLiveSchedule, liveSchedule.startTimeOfLiveSchedule);
 
   const { title } = useNavigation();
@@ -50,6 +52,7 @@ const NewCurationSubscriptionClubCard = ({ subscriptionClub, cardWidth, imgHeigh
     >
       <DisplayCardContent>
         <DisplayCardTitle>{name}</DisplayCardTitle>
+        <DisplayCardParagraph style={{ display: 'block' }}>{desc}</DisplayCardParagraph>
         <DisplayCardPlaceInfo>{place}</DisplayCardPlaceInfo>
         <DisplayCardMeetingInfo>{schedule}</DisplayCardMeetingInfo>
       </DisplayCardContent>
